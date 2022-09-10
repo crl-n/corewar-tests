69708b694774d3f685246c4853c109e865472016
Carl Nysten <nysten.carl@gmail.com>
2022-09-10 16:31:58 +0300

    basic checks improved

--- corewar-tests.py
+++ corewar-tests.py
@@ -6,12 +6,29 @@ from subprocess import run, PIPE
 def red(str):
     return "\033[31m" + str + "\033[0m"
 
+def green(str):
+    return "\033[32m" + str + "\033[0m"
+
 def check_basic_stuff(dir):
+    '''Checks Makefile, author-file and norm.'''
     files = [f for f in os.listdir(dir)]
+
     if "Makefile" not in files:
-        print("No Makefile found")
+        print(red("No Makefile found"))
+    else:
+        print(green("Makefile found"))
+
     if "author" not in files:
-        print("No author file found")
+        print(red("No author file found"))
+    else:
+        author_contents = run(["cat", dir + "/author"], stdout = PIPE)
+        print(green("Author file found:"), str(author_contents.stdout, 'utf-8').replace('\n', '\\n'))
+
+    norm_result = run(["norminette"], stdout = PIPE)
+    if norm_result.returncode != 1:
+        print(red("Norminette found norm errors!"))
+    else:
+        print(green("Norm OK"))
 
 def is_corewar_root_dir(dir: str):
     '''Checks if dir has a Makefile that mentions Corewar. If it does, it is interpreted
@@ -46,7 +63,7 @@ def main():
     if project_dir is None:
         print(red("Error: Couldn't locate Corewar directory"))
         return
-    print("Corewar directory found:", project_dir)
+    print(green("Corewar directory found:"), project_dir)
     check_basic_stuff(project_dir)
 
 if __name__ == '__main__':
