ee45a8d147959fb1f73a256055df202d1ade8125
Carl Nysten <nysten.carl@gmail.com>
2022-09-11 20:50:25 +0300

    resource retrieval split into function

--- corewar-tests.py
+++ corewar-tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 from subprocess import run, PIPE, DEVNULL
 from functools import reduce
 
@@ -13,32 +14,20 @@ def red(str):
 def green(str):
     return "\033[32m" + str + "\033[0m"
 
-def check_basic_stuff(dir):
-    '''Checks Makefile, author-file and norm.'''
-    files = [f for f in os.listdir(dir)]
-
-    if "Makefile" not in files:
-        print(red("No Makefile found"))
+def retrieve_resources():
+    if input(red("The official 42 Corewar resources could not be found.") + " Would you like to download them from intra.42.fr? (yes/no) ") in ['y', 'yes', 'Y' , 'Yes']:
+        curl_result = run(['curl', '--remote-name', 'https://projects.intra.42.fr/uploads/document/document/1170/vm_champs.tar'], stdout = DEVNULL, stderr = DEVNULL)
+        if curl_result.returncode != 0:
+            print(red("Error: Could not download resources. Check you network connection."))
+            sys.exit()
+        else:
+            print(green("Resources retrieved succesfully."))
+        run(["mkdir", "resources"])
+        run(["tar", "-xvf", "vm_champs.tar", "-C", "resources"], stdout = DEVNULL, stderr = DEVNULL)
+        run(["rm", "vm_champs.tar"])
     else:
-        print(green("Makefile found"))
-
-    if "author" not in files:
-        print(red("No author file found"))
-    else:
-        author_contents = run(["cat", dir + "/author"], stdout = PIPE)
-        print(green("Author file found:"), str(author_contents.stdout, 'utf-8').replace('\n', '\\n'))
-
-    norm_result = run(["norminette"], stdout = PIPE)
-    if norm_result.returncode != 1:
-        print(red("Norminette found norm errors!"))
-    else:
-        print(green("Norm OK"))
-
-def check_assembler():
-    pass
-
-def check_virtual_machine():
-    pass
+        print(red("Could not continue the execution of the script."))
+        sys.exit()
 
 def is_corewar_root_dir(dir: str):
     '''Checks if dir has a Makefile that mentions Corewar. If it does, it is interpreted
@@ -67,25 +56,40 @@ def find_project_dir(script_dir):
             return dir
     return None
 
+def check_basic_stuff(dir):
+    '''Checks Makefile, author-file and norm.'''
+    files = [f for f in os.listdir(dir)]
+
+    if "Makefile" not in files:
+        print(red("No Makefile found"))
+    else:
+        print(green("Makefile found"))
+
+    if "author" not in files:
+        print(red("No author file found"))
+    else:
+        author_contents = run(["cat", dir + "/author"], stdout = PIPE)
+        print(green("Author file found:"), str(author_contents.stdout, 'utf-8').replace('\n', '\\n'))
+
+    norm_result = run(["norminette"], stdout = PIPE)
+    if norm_result.returncode != 1:
+        print(red("Norminette found norm errors!"))
+    else:
+        print(green("Norm OK"))
+
+def check_assembler():
+    pass
+
+def check_virtual_machine():
+    pass
+
 def main():
     print(magenta("█▀▀ █▀█ █▀█ █▀▀ █░█░█ ▄▀█ █▀█   ▀█▀ █▀▀ █▀ ▀█▀ █▀\n█▄▄ █▄█ █▀▄ ██▄ ▀▄▀▄▀ █▀█ █▀▄   ░█░ ██▄ ▄█ ░█░ ▄█"))
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
 
     if "resources" not in os.listdir(script_dir):
-        if input(red("The official 42 Corewar resources could not be found.") + " Would you like to download them from intra.42.fr? (yes/no) ") in ['y', 'yes', 'Y' , 'Yes']:
-            curl_result = run(['curl', '--remote-name', 'https://projects.intra.42.fr/uploads/document/document/1170/vm_champs.tar'], stdout = DEVNULL, stderr = DEVNULL)
-            if curl_result.returncode != 0:
-                print(red("Error: Could not download resources from intra.42.fr. Check you network connection."))
-                return
-            else:
-                print(green("Resources retrieved."))
-            run(["mkdir", "resources"])
-            run(["tar", "-xvf", "vm_champs.tar", "-C", "resources"], stdout = DEVNULL, stderr = DEVNULL)
-            run(["rm", "vm_champs.tar"])
-        else:
-            print(red("Could not continue the execution of the script."))
-            return
+        retrieve_resources()
             
     project_dir = find_project_dir(script_dir)
     if project_dir is None:
