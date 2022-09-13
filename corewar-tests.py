af140627cec546088ca7315a027443bde2d27949
cnysten <cnysten@student.hive.fi>
2022-09-13 13:36:52 +0300

    shows what files are tested (asm)

--- corewar-tests.py
+++ corewar-tests.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import time
 from tempfile import NamedTemporaryFile
 from subprocess import run, PIPE, DEVNULL
 from functools import reduce
@@ -84,14 +85,28 @@ def check_assembler(resources_dir, project_dir):
     project_asm = project_dir + "/asm"
     official_asm = resources_dir + "/asm"
 
-    for f in [f for f in os.listdir(resources_dir + "/champs") if f.endswith(".s")]:
-        print(f)
+    if not os.path.exists(project_asm):
+        print(red("Error: Couldn't find your assembler."))
+        sys.exit()
+    if not os.path.exists(official_asm):
+        print(red("Error: Couldn't find the official assembler."))
+        sys.exit()
+
+    f = ""
+    for f in [resources_dir + '/champs/' + f for f in os.listdir(resources_dir + "/champs") if f.endswith(".s")]:
+        print("Comparing " + f, end = "\r")
         with NamedTemporaryFile() as temp_a, NamedTemporaryFile() as temp_b:
-            result_a = run([project_asm, f], stdout = PIPE)
-            result_b = run([official_asm, f], stdout = PIPE)
+            result_a = run([project_asm, f], stdout = PIPE, stderr = PIPE)
+            result_b = run([official_asm, f], stdout = PIPE, stderr = PIPE)
             temp_a.write(result_a.stdout)
             temp_b.write(result_b.stdout)
-            diff_result = run(["diff", "--report-identical-files", str(temp_a.name), str(temp_b.name)])
+            # temp_a.seek(0)
+            # temp_b.seek(0)
+            # print(temp_a.name, temp_b.name)
+            diff_result = run(["diff", "--report-identical-files", str(temp_a.name), str(temp_b.name)], stdout = PIPE)
+            time.sleep(0.03)
+            print("\033[2K", end = "")
+    print("Comparing " + f)
 
 def check_virtual_machine():
     print(magenta("\nVirtual machine tests"))
